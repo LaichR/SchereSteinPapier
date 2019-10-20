92e257e34457fb48611ab69791b332b885b4f306
LaichR <46196385+LaichR@users.noreply.github.com>
2019-10-20 11:07:33 +0200

    Dokumentation der Funktion EvalGame

--- Solution/Interface/SchereSteinPapierTools.cs
+++ Solution/Interface/SchereSteinPapierTools.cs
@@ -11,6 +11,15 @@ namespace SchereSteinPapierInterface
         public const string ArbiterService = "ShereSteinPapier.ArbiterService";
         public const string PlayerService = "ShereSteinPapier.PlayerService";
 
+        /// <summary>
+        /// EvalGame bestimmt, ob Player1 oder Player2 gewonnen hat. Falls
+        /// </summary>
+        /// <param name="player1Selection">Wahl des Player1</param>
+        /// <param name="player2Selection">Wahl des Player2</param>
+        /// <returns>Gewinner des Spiels:
+        /// - 1 falls Player1 gewonnen hat
+        /// - 2 falls Player2 gewonnen hat
+        /// - 0 im Fall eines Unentschieden</returns>
         public static int EvalGame(ESchereSteinPapier player1Selection, ESchereSteinPapier player2Selection)
         {
             if (player2Selection == player1Selection)
