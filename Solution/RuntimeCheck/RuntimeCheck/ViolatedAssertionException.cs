fddb8ebacabde8dec0c61041ac5e337a4d4dbc79
LaichR <46196385+LaichR@users.noreply.github.com>
2019-11-18 14:21:41 +0100

    make RuntimeCheck a local project

---
+++ Solution/RuntimeCheck/RuntimeCheck/ViolatedAssertionException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+
+
+
+namespace RuntimeCheck
+{
+    [Serializable]
+    public class ViolatedAssertionException : SystemException
+    {
+        const string StackTraceSerializationName = "AssertionStackTrace";
+        string _stackTrace;
+        internal ViolatedAssertionException(string message)
+            : base(message) {
+            var stack = new StackTrace(2); // i do not want to see the call frame of assert and this constructor!
+            _stackTrace = stack.ToString();
+        }
+        protected ViolatedAssertionException(SerializationInfo info, StreamingContext context)
+            : base(info, context) { _stackTrace = info.GetString(StackTraceSerializationName); }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+            info.AddValue(StackTraceSerializationName, _stackTrace);
+        }
+
+        public override string StackTrace => _stackTrace;
+        
+    }
+
+}
---
+++ Solution/RuntimeCheck/UnitTest/TestAssertionException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RuntimeCheck;
+using NUnit.Framework;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class TestAssertionException
+    {
+        [Test]
+        public void TestAssert()
+        {
+            try
+            {
+                RuntimeCheck.Assert.True(false, "this is a test assertion");
+            }
+            catch(ViolatedAssertionException ex)
+            {
+                var stackTrace = ex.StackTrace;
+                return;
+            }
+            NUnit.Framework.Assert.Fail();
+        }
+    }
+}
